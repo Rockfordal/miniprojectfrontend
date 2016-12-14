a1922b808420f09afa20cf303ca90b34965e2e9a
KlasClaywood <klas.claywood@gmail.com>
2016-12-14 13:55:58 +0100

    Mer htmlskelett

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,29 @@
 angular.module("app", [])
     .controller("testcontroller", function ($http, $scope) {
-        $scope.IPpath = "http://10.23.192.246:60000/api/test";
+        $scope.IPpath = "http://10.23.192.46:60000/api/test";
+        $scope.wordImageTest = {
+            title: "Bild - Ord",
+            description: "Skriv vad bilden föreställer",
+            showPart:1
+        }
+        $scope.separatorTest = {
+            title: "Skiljetecken",
+            description: "Skriv av texten med rätt skiljetecken",
+            showPart:1
+        }
+        $scope.colorTest = {
+            title: "Färger",
+            description: "Klicka på den färg som står",
+            showPart:1
+        }
+        $scope.textTest = {
+            title: "Bilda meningar",
+            description: "Skriv en mening med orden som visas",
+            showPart:1
+        }
+        $scope.showGame = 1;
         var getStrings = function () {
-            $http.get(IPpath)
+            $http.get($scope.IPpath)
                 .then(function (response) {
                     console.log(response.data);
                 }
