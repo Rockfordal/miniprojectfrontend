78e241fd3acf9bbab601c978160834e07dae993d
KlasClaywood <klas.claywood@gmail.com>
2016-12-14 09:06:30 +0100

    Lättare ändring av path till server

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,8 @@
 angular.module("app", [])
     .controller("testcontroller", function ($http, $scope) {
+        $scope.IPpath = "http://10.23.192.246:60000/api/test";
         var getStrings = function () {
-            $http.get("http://10.23.192.246:60000/api/test")
+            $http.get(IPpath)
                 .then(function (response) {
                     console.log(response.data);
                 }
