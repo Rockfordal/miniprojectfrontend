3b3ab435a42d64aae785bd77b0a957144c40dc39
KlasClaywood <klas.claywood@gmail.com>
2016-12-14 14:58:51 +0100

    Basic WordImage function

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,22 @@
 angular.module("app", [])
     .controller("testcontroller", function ($http, $scope) {
-        $scope.IPpath = "http://10.23.192.46:60000/api/test";
+        $scope.IPpath = "http://10.23.192.46:60000";
+        $scope.APIpath = "/api/test";
         $scope.wordImageTest = {
             title: "Bild - Ord",
             description: "Skriv vad bilden föreställer",
-            showPart:1
+            showPart:1,
+            showSubPart: 1,
+            
+            
+        }
+        $scope.currentImage = {
+            Image: "",
+            index: 0,
+        }
+        $scope.setNextImage = function(){
+            $scope.currentImage.index++;
+            $scope.currentImage.Image = $scope.Images[$scope.currentImage.index].Image;
         }
         $scope.separatorTest = {
             title: "Skiljetecken",
@@ -23,7 +35,7 @@ angular.module("app", [])
         }
         $scope.showGame = 1;
         var getStrings = function () {
-            $http.get($scope.IPpath)
+            $http.get($scope.IPpath + $scope.APIpath)
                 .then(function (response) {
                     console.log(response.data);
                 }
@@ -31,6 +43,26 @@ angular.module("app", [])
                     console.log("ErroR");
                     console.log(response)});
         }();
+        var getWordImageTest = function () {
+            $http.get($scope.IPpath + "/api/wordimagetest")
+            .then(function (response){
+                $scope.Images=response.data;
+                $scope.currentImage.Image = $scope.Images[0].Image;
+                console.log(response.data);
+            },
+                function(response){
+                console.log(response);
+            })
+        }
+        $scope.Run = function() {
+            getWordImageTest();
+        }
         $scope.title = "HEJ";
-       
+        $scope.reply = {
+            data: ["", "", "", "", ""]
+        };
+        $scope.response = {
+            data: [true, false, true, true, true]
+        };
+      
     })
