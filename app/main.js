2ad89d96a5577d9c915954b7d26e4757584413c9
Anders Lundström <lundstrom123@gmail.com>
2016-12-16 14:19:53 +0100

    halverst med colortest  anders+PÅ

--- app/main.js
+++ app/main.js
@@ -9,11 +9,9 @@ function ResultToArray(row) {
     return arr;
 }
 
-var serverurl = "http://10.23.192.71:60000";
-
 angular.module("app", [])
     .controller("testcontroller", function ($http, $scope) {
-        $scope.IPpath = serverurl;
+        $scope.IPpath = "http://10.23.192.71:60000";
 
         $scope.wordImageTest = {
             path: "/api/wordimagetest",
@@ -23,21 +21,24 @@ angular.module("app", [])
             replies: [{ Id: 0, Word: "" }, { Id: 0, Word: "" }, { Id: 0, Word: "" }, { Id: 0, Word: "" }, { Id: 0, Word: "" }],
             response: [false, true, false, false, false]
         }
+
         $scope.runWordImageTest = function () {
             $scope.showGame = 1;
             getWordImageTest();
         }
+
         $scope.currentImage = {
             Image: "",
             index: 0,
         }
+
         $scope.setNextImage = function () {
             if ($scope.currentImage.index < 4) {
                 $scope.currentImage.index++;
                 $scope.currentImage.Image = $scope.Images[$scope.currentImage.index].Image;
             }
             else {
-                var apiurl = $scope.IPpath + $scope.wordImageTest.path, data;
+                var apiurl = $scope.IPpath + $scope.wordImageTest.path;
                 var data = $scope.wordImageTest.replies;
                 // var rawdata = [{Id:1,"Word":"fågel"},{Id:2, Word: "bil"},{Id:3,"Word":"katt"},{Id:4,"Word":"hund"},{Id:5,"Word":"sdfsdf"}];
 
@@ -52,11 +53,13 @@ angular.module("app", [])
                 $scope.currentImage.index = 0;
             }
         }
+
         $scope.separatorTest = {
             title: "Skiljetecken",
             description: "Skriv av texten med rätt skiljetecken",
             showPart: 1
         }
+
         $scope.colorTest = {
             title: "Färger",
             description: "Klicka på den färg som står",
@@ -66,16 +69,19 @@ angular.module("app", [])
             alternatives: [{ Id: 3, hex: "#00ffff" }, { Id: 86, hex: "#ff0000" }, { Id: 36, hex: "#00ff00" }, { Id: 17, hex: "#ff0000" }, { Id: 16, hex: "#00ff00" }, { Id: 23, hex: "#ff0000" }, { Id: 24, hex: "#00ff00" }],
             response: [true, false, false, true, true]
         }
+
         $scope.currentColor = {
             index: 0,
             text: "BRUN",
         }
+
         $scope.Colors = [{ Id: 5, text: "Brun" }, { Id: 2, text: "Grön" }, { Id: 2, text: "Blå" }, { Id: 2, text: "Röd" }, { Id: 2, text: "Saffran" }];
         $scope.textTest = {
             title: "Bilda meningar",
             description: "Skriv en mening med orden som visas",
             showPart: 1
         }
+
         $scope.showGame = 0;
 
         var getWordImageTest = function () {
@@ -94,13 +100,29 @@ angular.module("app", [])
                 });
         }
 
+        var getColorTest = function () {
+            console.log("Getting Color test");
+            $http.get($scope.IPpath + $scope.colorTest.path)
+                .then(function (response) {
+                    $scope.colorTest.alternatives = reponse.data.alternatives;
+                    $scope.currentColor.index = response.data.Id;
+                    $scope.currentColor.text = response.data.Name;
+                    console.log('get colortest: ', response.data);
+                },
+                function (response) {
+                    console.log(response);
+                });
+        }
         $scope.title = "HEJ";
+
         $scope.reply = {
             data: ["", "", "", "", ""]
         };
+
         $scope.response = {
             data: [true, false, true, true, true]
         };
+
         $scope.selectSwatch = function (index) {
             $scope.colorTest.replies[$scope.currentColor.index].Id = $scope.colorTest.alternatives[index].Id;
             $scope.colorTest.replies[$scope.currentColor.index].hex = $scope.colorTest.alternatives[index].hex;
@@ -109,17 +131,20 @@ angular.module("app", [])
             $("#swatch" + index).css("border", "5px solid black");
             console.log($("#swatch" + index).css("border"));
         }
+
         $scope.setNextColor = function () {
             if ($scope.currentColor.index < 4) {
                 $scope.currentColor.index += 1;
                 $scope.currentColor.text = $scope.Colors[$scope.currentColor.index].text;
             }
             else {
-                console.log($scope.colorTest.replies);
-                $http.post($scope.IPpath + $scope.colorTest.path, $scope.colorTest.replies)
+                console.log("replies: ", $scope.colorTest.replies);
+                var apiurl = $scope.IPpath + $scope.colorTest.path;
+                $http.post(apiurl, $scope.colorTest.replies)
                     .then(function (response) {
-                        console.log(response.data);
-                        $scope.colorTest.response = response.data;
+                        console.log("data: ", response.data);
+                        // $scope.colorTest.response = response.data;
+                        $scope.colorTest.response = ResultToArray(response.data)
                         $scope.colorTest.showPart = 3;
                     }, function (response) {
                         console.log(response);
@@ -127,4 +152,4 @@ angular.module("app", [])
                     });
             }
         }
-    })
+})
